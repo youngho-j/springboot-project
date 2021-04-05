fa660c3aedd34b461339f0701c05ba8dab21ac38
youngho-j <alfkwl1239@naver.com>
2021-04-06 01:40:13 +0900

    gradle 다운그레이드 및 Dto 테스트, java version 변경

---
+++ src/test/java/com/youngho/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.youngho.book.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponesDto dto = new HelloResponesDto(name, amount);
+
+        //then
+        // assertThat - assertj 라이브러리의 검증 메서드 / 메서드 체이닝 지원(메서드를 이어붙여서 사용가능함)
+        // isEqualTo - assertj 라이브러리의 동등 비교 메서드
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
