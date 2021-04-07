b60ecdb224e2f11874b3148e75c51a1df7be637e
youngho-j <alfkwl1239@naver.com>
2021-04-07 17:24:47 +0900

    프로젝트 update 기능 추가

--- src/main/java/com/youngho/book/springboot/service/PostsService.java
+++ src/main/java/com/youngho/book/springboot/service/PostsService.java
@@ -1,8 +1,11 @@
 package com.youngho.book.springboot.service;
 
+import com.youngho.book.springboot.domain.posts.Posts;
 import com.youngho.book.springboot.domain.posts.PostsRepository;
+import com.youngho.book.springboot.web.dto.PostsResponseDto;
 import com.youngho.book.springboot.web.dto.PostsSaveRequestDto;
 
+import com.youngho.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,4 +19,43 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        /*
+        *   보통 update 기능을 만들면 데이터베이스에 쿼리를 날려야하는데 여기에서는 날리는 부분이 없음..
+        *
+        *   어떻게 이러한 상황이 가능한가?
+        *   JPA의 영속성 컨텍스트 덕분
+        *
+        *   그럼 영속성 컨텍스트는 무엇인가?
+        *   엔티티를 영구 저장하는 환경이며, 일종의 논리적 개념으로 보면됨
+        *
+        *   JPA의 핵심 내용은 엔티티가 영속성 컨텍스트에 포함되어 있냐 아니냐로 갈림
+        *
+        *   영속성 컨텍스트에 포함되어있다는 것은
+        *   JPA의 엔티티 매니저가 활성화된 상태(Spring Data Jpa 사용시는 기본적으로 적용되어있음)로
+        *   트랜잭션 안에서 데이터베이스에서 데이터를 가져오는 경우 데이터는 영속성 컨텍스트가 유지된 상태임
+        *
+        *   이 상태에서 해당 데이터의 값을 변경시 트랜잭션이 끝나는 시점에 해당 테이블의 변경 분을 반영
+        *
+        *   즉, Entity 객체의 값만 변경하면 별도로 update 쿼리를 날릴 필요가 없음
+        *
+        *   이러한 개념을 더티체킹(상태 변경 검사)이라고 함
+        * */
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
 }
--- src/main/java/com/youngho/book/springboot/web/PostsApiController.java
+++ src/main/java/com/youngho/book/springboot/web/PostsApiController.java
@@ -1,12 +1,17 @@
 package com.youngho.book.springboot.web;
 
 import com.youngho.book.springboot.service.PostsService;
+import com.youngho.book.springboot.web.dto.PostsResponseDto;
 import com.youngho.book.springboot.web.dto.PostsSaveRequestDto;
+import com.youngho.book.springboot.web.dto.PostsUpdateRequestDto;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /*
 *   @RequiredArgsConstructor - final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성
@@ -22,9 +27,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PostsApiController {
     private final PostsService postsService;
-
+    
+    //등록 기능
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
