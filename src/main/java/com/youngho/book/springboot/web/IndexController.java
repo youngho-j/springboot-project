fe9d6161594095690795f83e89fd6fbcede6a15d
youngho-j <alfkwl1239@naver.com>
2021-04-09 15:23:25 +0900

    메인, 등록 페이지 추가 1. 메인 페이지 layout 추가 2. 글 등록 페이지 추가

--- src/main/java/com/youngho/book/springboot/web/IndexController.java
+++ src/main/java/com/youngho/book/springboot/web/IndexController.java
@@ -3,10 +3,17 @@ package com.youngho.book.springboot.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//페이지에 관련된 컨트롤러
 @Controller
 public class IndexController {
+    //메인 페이지로 이동
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+    //글 등록 페이지로 이동
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
