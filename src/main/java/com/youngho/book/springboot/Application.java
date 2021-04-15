5e1712b938e442f9feaa1620ce17c7df6f31995b
youngho-j <alfkwl1239@naver.com>
2021-04-15 15:45:20 +0900

    테스트에 시큐리티 적용하기
    1. HelloControllerTest - SecurityConfig를 스캔대상에서 제거하여 CustomOAuth@UserService를 못읽어오는 에러 제거 / 테스트용 인증된 사용자 추가
    
    2. JpaConfig - @EnableAuditing으로 인해 발생하는 에러를 해결하기 위해 @SpringBootApplication
                   을 분리
    
    3. Application - 테스트 환경에서는 WebMvcTest로 Entity 클래스가 없으므로 @EnableAuditing를 제거
    
    3. PostsApiControllerTest - 필요없는 import 코드 제거

--- src/main/java/com/youngho/book/springboot/Application.java
+++ src/main/java/com/youngho/book/springboot/Application.java
@@ -8,7 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 // @SpringBootApplication - 스프링 부트의 자동 설정, 스프링 Bean 읽기 생성 모두 자동설정
 // @SpringBootApplication 선언 위치부터 설정을 읽어감 따라서, 프로젝트 최상단에 위치해야함
 // @EnableJpaAuditing - JPA Auditing 어노테이션을 모두 활성화 할 수 있도록 함
-@EnableJpaAuditing
+
+// @EnableJpaAuditing - 제거(테스트시 사용을 위해 @Entity 클래스가 최소 하나가 필요한데 
+// @WebMvcTest엔 없으므로, @SpringBootApplication와 분리하여 작성
+// 
 @SpringBootApplication
 public class Application {
     // 프로젝트의 메인 클래스
--- src/test/java/com/youngho/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/youngho/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.youngho.book.springboot.web;
 
+import com.youngho.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,25 +19,40 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 // @RunWith - 테스트 진행시 JUnit에 내장된 실행자 외 다른 실행자 실행 즉, 스프링 부트 테스트와 JUnit의 연결자 역할
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+// 스캔 대상에서 SecurityConfig 제거
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
     // @Autowired - 스프링이 관리하는 bean 주입 받음
     // MockMvc - 웹 API를 테스트시 사용(HTTP, GET, POST 등), 스프링 MVC 테스트의 시작점
+
+    /*
+    *   No qualifying bean of type 'com.youngho.book.springboot.config.auth.CustomOAuth2UserService'
+    *   발생 원인 : @WebMvcTest는 CustomOAuth2UserService를 스캔하지 않음
+    *   @WebMvcTest 스캔 대상 : WebSecurityConfigurerAdapter, WebMvcConfigurer를 비롯한 @ControllerAdvice, @Controller
+    *   즉, @Repository, @Service, @Component는 스캔 대상이 아님
+    *   따라서, SecurityConfig는 읽었지만, SecurityConfig를 생성하기 위해 필요한 CustomOAuth2UserService를 읽을 수 없어 발생
+    * */
+
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         /* mvc.perform(get("/hello")) - MockMvc를 통해 HTTP GET 요청
-        *  .andExpect(status().isOk()) - mvc.perform의 결과 검증 즉, HTTP Header의 Status를 검증 여기서는 200인지 아닌지 검사
-        *  .andExpect(content().string(hello)) - mvc.perform의 결과 검증 즉, 응답 본분의 내용 검증 여기서는 hello 값이 맞는지 검사
-        * */
+         *  .andExpect(status().isOk()) - mvc.perform의 결과 검증 즉, HTTP Header의 Status를 검증 여기서는 200인지 아닌지 검사
+         *  .andExpect(content().string(hello)) - mvc.perform의 결과 검증 즉, 응답 본분의 내용 검증 여기서는 hello 값이 맞는지 검사
+         * */
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
-
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
