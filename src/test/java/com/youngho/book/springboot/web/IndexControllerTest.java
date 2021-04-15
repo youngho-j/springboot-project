e7c15732b39644d196940de71f10936d27cd06ee
youngho-j <alfkwl1239@naver.com>
2021-04-15 15:46:40 +0900

    테스트에 시큐리티 적용하기
    1. index.mustache - 개발 당시 화면 부분과 화면 테스트시 테스트 할 내용 차이로 인해 오류 발생
                        테스트 내용과 개발 화면 출력 부분 일치 시킴

--- src/test/java/com/youngho/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/youngho/book/springboot/web/IndexControllerTest.java
@@ -23,6 +23,11 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/",String.class);
 
         //then - URL 호출시 페이지의 내용이 제대로 호출되는지 확인(전체코드를 다 검증할 필요는 없음)
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스 Ver.2");
+        /*
+        *   테스트 시 오류 발생 - 원인 : body부분에 해당 스프링 부트로 시작하는 웹 서비스 Ver.2 해당 내용이
+        *   존재하지 않아서 발생, 초기에 스프링 부트로 시작하는 웹 서비스로 테스트를 수행하고 결과를 확인 했으나
+        *   이후  index 수정당시 메인페이지 h1 태그 이름을 변경했는데 테스트 코드에는 적용을 하지 않아서 발생
+        * */
     }
 }
